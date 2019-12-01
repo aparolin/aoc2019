6e5fa4802ab6eb22090b6b7ab56066bcf3b8d06b
Alessandro Parolin <alessandro.parolin@gmail.com>
2019-12-01 19:18:02 -0300

    remove functools

--- 01/main.py
+++ 01/main.py
@@ -1,5 +1,4 @@
 import math
-import functools
 
 def fuel_required(mass):
   return math.floor(mass/3) - 2
@@ -21,7 +20,7 @@ def sum_fuel_requirements(input_file, part1):
   else:
     fuel_requirements = list(map(fuel_required_with_additional_fuel, masses))
 
-  return functools.reduce(lambda a,b:a+b, fuel_requirements)
+  return sum(fuel_requirements)
 
 if __name__ == '__main__':
   part1_result = sum_fuel_requirements('input.txt', True)
