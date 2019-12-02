bf1dc0b683909a8faf4432734cf90b3f5ec38a40
Alessandro Parolin <alessandro.parolin@sap.com>
2019-12-02 15:20:51 -0300

    day 2 part 2

--- src/02/main.py
+++ src/02/main.py
@@ -1,29 +1,57 @@
-def run_program(input_file):
-    int_codes = list(map(int, open(input_file).read().split(',')))
-
+def run_program(memory):
     pointer = 0
 
     while True:
-        op_code = int_codes[pointer]
+        op_code = memory[pointer]
         if op_code == 99:
             break
 
-        value1_idx = int_codes[pointer+1]
-        value2_idx = int_codes[pointer+2]
-        result_idx = int_codes[pointer+3]
+        value1_idx = memory[pointer+1]
+        value2_idx = memory[pointer+2]
+        result_idx = memory[pointer+3]
 
         if op_code == 1:
-            result = int_codes[value1_idx] + int_codes[value2_idx]
+            result = memory[value1_idx] + memory[value2_idx]
         elif op_code == 2:
-            result = int_codes[value1_idx] * int_codes[value2_idx]
+            result = memory[value1_idx] * memory[value2_idx]
         else:
             raise Exception(f'Unexpected opcode: {op_code}')
-        int_codes[result_idx] = result
-
+        memory[result_idx] = result
         pointer += 4
 
-    return int_codes
+    return memory[0]
+
+def run_part1(memory):
+    memcopy = memory[:]
+    memcopy[1] = 12
+    memcopy[2] = 2
+    return run_program(memcopy)
+
+def run_part2(memory):
+    expected_result_part2 = 19690720
+    done = False
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            memcopy = memory[:]
+
+            memcopy[1] = noun
+            memcopy[2] = verb
+
+            if run_program(memcopy) == expected_result_part2:
+                result_part2 = (100 * noun) + verb
+                done = True
+                break
+
+        if done:
+            break
+
+    return result_part2
 
 if __name__ == '__main__':
-    result_part1 = run_program('input.txt')
-    print(f'Part 1: {result_part1[0]}')
+    memory = list(map(int, open('input.txt').read().split(',')))
+
+    print(f'Part 1: {run_part1(memory)}')
+    print(f'Part 2: {run_part2(memory)}')
+
+# 205800 => too high
+# 9821 => too high
