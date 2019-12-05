941894817f2fea0c04cf8f20c1007fbf3067d52a
Alessandro Parolin <alessandro.parolin@sap.com>
2019-12-05 16:50:48 -0300

    day 5 part 2

--- src/05/main.py
+++ src/05/main.py
@@ -13,18 +13,22 @@ def init_memory(instructions):
     mem[idx] = instruction
   return mem
 
-def get_params_3(mem, ip, modes):
+def get_n_params(n, mem, ip, modes):
   params = []
-  params.append(mem[mem[ip+1]] if modes[0] == 0 else mem[ip+1])
-  params.append(mem[mem[ip+2]] if modes[1] == 0 else mem[ip+2])
-  params.append(mem[ip+3])
+
+  for i in range(0, n):
+    #write address parameter
+    if i >= len(modes):
+      params.append(mem[ip+i+1])
+    else:
+      params.append(mem[mem[ip+1+i]] if modes[i] == 0 else mem[ip+1+i])
+  
   return params
 
-def run_program(instructions):
+def run_program(instructions, input_code):
   mem = init_memory(instructions)
 
   ip = 0
-  cur_input = 1
   while True:
     inst = mem[ip]
     [op_code, modes] = parse_instruction(inst)
@@ -32,29 +36,55 @@ def run_program(instructions):
     if op_code == 99:
       break
     elif op_code == 1:
-      params = get_params_3(mem, ip, modes)
+      params = get_n_params(3, mem, ip, modes)
       mem[params[2]] = params[0] + params[1]
       ip += 4
     elif op_code == 2:
-      params = get_params_3(mem, ip, modes)
+      params = get_n_params(3, mem, ip, modes)
       mem[params[2]] = params[0] * params[1]
       ip += 4
     elif op_code == 3:
       param = mem[ip+1]
-      mem[param] = cur_input
+      mem[param] = input_code
       ip += 2
     elif op_code == 4:
       param = mem[ip+1]
       print(mem[param])
       ip += 2
+    elif op_code == 5:
+      params = get_n_params(2, mem, ip, modes)
+      if params[0] != 0:
+        ip = params[1]
+      else:
+        ip+=3
+    elif op_code == 6:
+      params = get_n_params(2, mem, ip, modes)
+      if params[0] == 0:
+        ip = params[1]
+      else:
+        ip+=3
+    elif op_code == 7:
+      params = get_n_params(3, mem, ip, modes)
+      if params[0] < params[1]:
+        mem[params[2]] = 1
+      else:
+        mem[params[2]] = 0
+      ip += 4
+    elif op_code == 8:
+      params = get_n_params(3, mem, ip, modes)
+      if params[0] == params[1]:
+        mem[params[2]] = 1
+      else:
+        mem[params[2]] = 0
+      ip += 4
     else:
       raise Exception(f'Unexpected code {op_code}')
-
-def run_part1(instructions):
-  run_program(instructions)
 
 if __name__ == '__main__':
   instructions = list(map(int, open('input.txt').read().split(',')))
 
   print('Part 1:')
-  run_part1(instructions)
+  run_program(instructions, 1)
+
+  print('Part 2:')
+  run_program(instructions, 5)
