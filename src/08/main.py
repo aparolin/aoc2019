28956d68403206a64871a3cea8e848056d23b919
Parolin <alessandro.parolin@sap.com>
2019-12-08 22:35:58 -0300

    day 08 part 2

--- src/08/main.py
+++ src/08/main.py
@@ -16,7 +16,7 @@ def find_layers(pixels, width, height):
   offset = 0
 
   while pixel_idx < len(pixels)-1:
-    layer = [[0 for c in range(width)] for w in range(height)]
+    layer = [[0 for c in range(width)] for r in range(height)]
     total_zero_digits = 0
 
     for row in range(height):
@@ -44,8 +44,32 @@ def run_part1(layers):
   total_twos = get_total_digits(2, layers[layer_fewest_zero_digits])
   print(f'Part 1: {total_ones * total_twos}')
 
+def run_part2(layers):
+  cols = len(layers[0][0])
+  rows = len(layers[0])
+  image = [[0 for c in range(cols)] for w in range(rows)]
+
+  for row in range(rows):
+    for col in range(cols):
+      for l in layers:
+        if l[row][col] == 2:
+          continue
+        else:
+          image[row][col] = l[row][col]
+          break
+
+  print('Part 2:\n')
+  for row in range(rows):
+    for col in range(cols):
+      char = image[row][col]
+      if char == 0:
+        print('.', end='')
+      else:
+        print('o', end='')
+    print()
+
 if __name__ == '__main__':
   pixels = list(map(int,open('input.txt').read()))
   layers = find_layers(pixels, 25, 6)
   run_part1(layers)
-  
+  run_part2(layers)
